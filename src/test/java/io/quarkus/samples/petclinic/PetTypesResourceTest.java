0d7af26b2233ae258fce0b823f4c5a91889b43b9
Andrey Belyaev <belyaev.andrey@gmail.com>
2019-10-24 10:56:53 +0300

    Updated version to 0.26.1, migrated to new security layer

--- src/test/java/io/quarkus/samples/petclinic/PetTypesResourceTest.java
+++ src/test/java/io/quarkus/samples/petclinic/PetTypesResourceTest.java
@@ -1,8 +1,6 @@
 package io.quarkus.samples.petclinic;
 
-import io.quarkus.samples.petclinic.model.PetType;
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.mapper.ObjectMapperType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -62,44 +60,4 @@ public class PetTypesResourceTest {
                 .then()
                 .statusCode(401);
     }
-
-
-    @Test
-    public void testAddNewType() {
-        given()
-                .when()
-                .auth().preemptive().basic("admin", "admin")
-                .body(new PetType(7, "crocodile"), ObjectMapperType.GSON)
-                .contentType("application/json;charset=utf-8")
-                .post("/api/pettypes")
-                .then()
-                .statusCode(201)
-                .header("Location", containsString("/api/pettypes/7"));
-    }
-
-
-    @Test
-    public void testUpdateType() {
-        given()
-                .when()
-                .auth().preemptive().basic("admin", "admin")
-                .body(new PetType(1, "The Cat"), ObjectMapperType.GSON)
-                .contentType("application/json;charset=utf-8")
-                .put("/api/pettypes/1")
-                .then()
-                .statusCode(204);
-
-        given()
-                .when()
-                .auth().preemptive().basic("admin", "admin")
-                .get("api/pettypes/1")
-                .then()
-                .statusCode(200)
-                .body(
-                        containsString("The Cat")
-                );
-
-    }
-
-
 }
